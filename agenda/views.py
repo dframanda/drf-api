84638130cfc9eee354f5c14fd649cbd01ecdc43c
Amanda Regnel <amandaregnel@gmail.com>
2022-03-22 14:15:40 -0300

    Adiciona método POST à listagem de agendamentos

--- agenda/views.py
+++ agenda/views.py
@@ -12,8 +12,22 @@ def agendamento_detail(request, id):
     #atributos da classe Agendamento no serializers (por isso mantemos os mesmos nomes)
     return JsonResponse(serializer.data)
     
-@api_view(http_method_names=["GET"])
+@api_view(http_method_names=["GET", "POST"])
 def agendamento_list(request):
-    qs = Agendamento.objects.all() #queryset
-    serializer = AgendamentoSerializer(qs, many=True)
-    return JsonResponse(serializer.data, safe=False)
+    if request.method == "GET":
+        qs = Agendamento.objects.all() #queryset
+        serializer = AgendamentoSerializer(qs, many=True)
+        return JsonResponse(serializer.data, safe=False)
+    if request.method == "POST":
+        data = request.data # {"nome_cliente": "Gabriel" ...}
+        serializer = AgendamentoSerializer(data=data)
+        if serializer.is_valid():
+            validated_data = serializer.validated_data
+            Agendamento.objects.create(
+                data_horario=validated_data["data_horario"],
+                nome_cliente=validated_data["nome_cliente"],
+                email_cliente=validated_data["email_cliente"],
+                telefone_cliente=validated_data["telefone_cliente"],
+            )
+            return JsonResponse(serializer.data, status=201) #status 201 significa created
+        return JsonResponse(serializer.errors, status=400)
