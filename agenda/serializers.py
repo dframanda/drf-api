da4b1df917e46ee26a4b2c3be0cb57da8c4ab4c0
Amanda Regnel <amandaregnel@gmail.com>
2022-03-30 14:57:41 -0300

    Implementa regras de negócio

--- agenda/serializers.py
+++ agenda/serializers.py
@@ -1,7 +1,80 @@
+import email
+import queue
+from time import time
+from urllib import request
 from rest_framework import serializers
+from django.utils import timezone
+from agenda.models import Agendamento
+import re
+from datetime import datetime, timedelta, tzinfo
 
-class AgendamentoSerializer(serializers.Serializer):
-    data_horario = serializers.DateTimeField()
-    nome_cliente = serializers.CharField(max_length=200)
-    email_cliente = serializers.EmailField()
-    telefone_cliente = serializers.CharField(max_length=20)
+class AgendamentoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Agendamento
+        fields = ['id', 'data_horario', 'nome_cliente', 'email_cliente', 'telefone_cliente']
+
+    def validate_data_horario(self, value):
+        if value < timezone.now():
+            raise serializers.ValidationError("Agendamento não pode ser feito no passado!")
+        
+        if value:
+            data_request = datetime.fromisoformat(str(value))
+            queryset = Agendamento.objects.all()
+            timeDelta = timedelta(minutes=30)
+
+            inicio_pausa = datetime(data_request.year, data_request.month, data_request.day, 12, 00, tzinfo=timezone.utc)
+            fim_pausa = datetime(data_request.year, data_request.month, data_request.day, 13, 00, tzinfo=timezone.utc)
+            inicio_expediente = datetime(data_request.year, data_request.month, data_request.day, 9, 00, tzinfo=timezone.utc)
+            fim_expediente = datetime(data_request.year, data_request.month, data_request.day, 18, 00, tzinfo=timezone.utc)
+            fim_expediente_sabado = datetime(data_request.year, data_request.month, data_request.day, 13, 00, tzinfo=timezone.utc)
+
+            if data_request.weekday() != 6 and data_request.weekday() != 5 and data_request >= inicio_pausa and data_request <= fim_pausa:
+                raise serializers.ValidationError('Não é possível marcar um agendamento no horário de almoço!')
+            if data_request.weekday() == 6:
+                raise serializers.ValidationError('O estabelecimento não funciona aos domingos!')
+            if data_request.weekday() != 5 and data_request >= fim_expediente:
+                raise serializers.ValidationError('O estabelecimento funciona apenas até 18h!')
+            if data_request.weekday() == 5 and data_request > fim_expediente_sabado:
+                raise serializers.ValidationError('O estabelecimento funciona somente até 13h aos sábados!')
+            if data_request < inicio_expediente:
+                raise serializers.ValidationError('O estabelecimento abre apenas às 9h!')
+
+            for e in queryset:
+                date_intern = datetime.date(e.data_horario)
+                
+                if date_intern == data_request.date():
+                    if e.data_horario <= data_request - timeDelta or e.data_horario >= data_request + timeDelta:
+                        pass
+                    else:
+                        raise serializers.ValidationError('O horário selecionado não está disponível!')
+
+        return value
+        
+    def validate(self, attrs):
+        nome_cliente = attrs.get("nome_cliente", "")
+        data_horario = attrs.get("data_horario", "")
+        telefone_cliente = attrs.get("telefone_cliente", "") 
+        email_cliente = attrs.get("email_cliente", "")
+
+        if data_horario and email_cliente:
+            if Agendamento.objects.filter(nome_cliente=nome_cliente, email_cliente=email_cliente, data_horario__date=data_horario).exists():
+                raise serializers.ValidationError('Cada cliente pode ter apenas uma reverva por dia!')
+
+        if email_cliente.endswith(".br") and telefone_cliente.startswith("+") and not telefone_cliente.startswith("+55"): 
+            raise serializers.ValidationError("E-mail brasileiro deve estar associado a um número do Brasil (+55)")
+
+        return attrs
+    
+    def validate_telefone_cliente(self, value):
+        if len(value) < 8:
+            raise serializers.ValidationError("Telefone precisa conter no mínimo 8 dígitos")
+        if "+" in value and not value.startswith("+"):
+            raise serializers.ValidationError("Formato inválido")
+        if not re.sub(r'[^0-9]', '', value).isdigit():
+            raise serializers.ValidationError("Telefone deve ser composto por dígitos!")
+        return value
+    
+    
+
+
+    
--- agenda/views.py
+++ agenda/views.py
@@ -3,31 +3,99 @@ from django.shortcuts import get_object_or_404
 from agenda.models import Agendamento
 from agenda.serializers import AgendamentoSerializer
 from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
+from datetime import date, datetime, timedelta
 
 # Create your views here.
-@api_view(http_method_names=["GET"])
+@api_view(http_method_names=["GET", "PATCH", "DELETE"])
 def agendamento_detail(request, id):
     obj = get_object_or_404(Agendamento, id=id)
-    serializer = AgendamentoSerializer(obj) #representacao json, ele vai buscar encontrar nesses campos os mesmos 
-    #atributos da classe Agendamento no serializers (por isso mantemos os mesmos nomes)
-    return JsonResponse(serializer.data)
-    
+    obj.cancelado = False
+    if request.method == "GET":
+        serializer = AgendamentoSerializer(obj)
+        return JsonResponse(serializer.data)
+    if request.method == "PATCH":
+        serializer = AgendamentoSerializer(obj, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return JsonResponse(serializer.data, status=200)
+        return JsonResponse(serializer.errors, status=400)
+    if request.method == "DELETE":
+        obj.cancelado = True
+        lista_cancelados = obj.delete()
+        return Response(status=204)
+
 @api_view(http_method_names=["GET", "POST"])
 def agendamento_list(request):
     if request.method == "GET":
-        qs = Agendamento.objects.all() #queryset
+        qs = Agendamento.objects.all()
         serializer = AgendamentoSerializer(qs, many=True)
         return JsonResponse(serializer.data, safe=False)
     if request.method == "POST":
-        data = request.data # {"nome_cliente": "Gabriel" ...}
+        data = request.data
         serializer = AgendamentoSerializer(data=data)
         if serializer.is_valid():
-            validated_data = serializer.validated_data
-            Agendamento.objects.create(
-                data_horario=validated_data["data_horario"],
-                nome_cliente=validated_data["nome_cliente"],
-                email_cliente=validated_data["email_cliente"],
-                telefone_cliente=validated_data["telefone_cliente"],
-            )
-            return JsonResponse(serializer.data, status=201) #status 201 significa created
+            serializer.save()
+            return JsonResponse(serializer.data, status=201)
         return JsonResponse(serializer.errors, status=400)
+
+@api_view(http_method_names=["GET"])
+def horarios_list(request, data):
+
+    data = datetime.strptime(data, '%Y-%m-%d').date()
+    qs = Agendamento.objects.filter(data_horario__date=data).order_by('data_horario__time')
+    serializer = AgendamentoSerializer(qs, many=True)
+    delta = timedelta(minutes=30)
+
+    horarios_list = []
+
+    abertura = datetime(data.year, data.month, data.day, 9, 00)
+    encerramento = datetime(data.year, data.month, data.day, 18, 00)
+    encerramento_sabado = datetime(data.year, data.month, data.day, 13, 00)
+    horario_de_almoco = datetime(data.year, data.month, data.day, 12, 00)
+    volta_do_almoco = datetime(data.year, data.month, data.day, 13, 00)
+
+    if data.weekday() == 6:
+        horarios_list.append({
+            'Informação': 'O estabelecimento não abre aos domingos.'
+        })
+
+    if data.weekday() == 5:
+        while abertura != encerramento_sabado:
+            horarios_list.append({
+                'data_horario': abertura
+            })
+
+            abertura += delta
+
+    if data.weekday() != 5 and data.weekday() != 6:
+        while abertura != encerramento:
+            horarios_list.append({
+                'data_horario': abertura
+            })
+
+            if abertura == horario_de_almoco:
+                if horario_de_almoco < volta_do_almoco:
+                    horarios_list.pop()
+                    horario_de_almoco += delta
+
+            abertura += delta
+
+    if horario_de_almoco in horarios_list:
+        while horario_de_almoco != volta_do_almoco:
+            horarios_list.remove(horario_de_almoco)
+
+            horario_de_almoco += delta
+
+    for e in serializer.data:
+        e = e.get('data_horario')
+        horario_e = e[11:16]
+        for time in horarios_list:
+            data_horario = time.get('data_horario')
+            horario_list = datetime.strftime(data_horario, '%H:%M')
+            if horario_e == horario_list:
+                horarios_list.remove(time)
+
+        return JsonResponse(horarios_list, safe=False)    
+    return JsonResponse(serializer.errors, status=400)
