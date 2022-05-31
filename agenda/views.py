dd28384de521e3ad44b4d3bd3e76accd8e840b29
dframanda <amandaregnel@gmail.com>
2022-05-30 22:09:40 -0300

    Corrige importação Response

--- agenda/views.py
+++ agenda/views.py
@@ -5,7 +5,7 @@ from django.utils import timezone
 
 from django.contrib.auth.models import User
 from django.http import JsonResponse
-from requests import Response
+from rest_framework.response import Response
 from rest_framework import generics, permissions
 from rest_framework.decorators import api_view
 
